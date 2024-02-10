0446d0e0bea44a6078f1304d3b1f2bfeedcf95b8
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-02-10 18:27:48 +0530

    navbar created

---
+++ src/components/nav/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Flex, Box, Text } from '@chakra-ui/react';
+
+const Navbar = () => {
+  return (
+    <Flex>
+      <Box>
+        <Text>Rick's Photography</Text>
+      </Box>
+      <Box></Box>
+    </Flex>
+  );
+};
+
+export default Navbar;
