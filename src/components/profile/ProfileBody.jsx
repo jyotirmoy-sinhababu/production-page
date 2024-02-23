2f76cdde37eb52c6958c71add5593d3d4a140065
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-02-23 11:36:32 +0530

    changes made on profileBody

--- src/components/profile/ProfileBody.jsx
+++ src/components/profile/ProfileBody.jsx
@@ -1,39 +1,87 @@
-import { GridItem, Image, Flex } from '@chakra-ui/react';
+import {
+  GridItem,
+  Image,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 const ProfileBody = ({ data }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   console.log(data);
   return (
-    <GridItem
-      cursor={'pointer'}
-      borderRadius={4}
-      overflow={'hidden'}
-      border={'1px solid'}
-      borderColor={'whiteAlpha.300'}
-      position={'relative'}
-      aspectRatio={1 / 1}
-    >
-      <Flex
-        opacity={0}
-        _hover={{ opacity: 1 }}
-        position={'absolute'}
-        top={0}
-        left={0}
-        right={0}
-        bottom={0}
-        bg={'blackAlpha.700'}
-        transition={'all 0.3s ease'}
-        zIndex={1}
-        justifyContent={'center'}
-      ></Flex>
+    <>
+      {' '}
+      <GridItem
+        cursor={'pointer'}
+        borderRadius={4}
+        overflow={'hidden'}
+        border={'1px solid'}
+        borderColor={'whiteAlpha.300'}
+        position={'relative'}
+        aspectRatio={1 / 1}
+        onClick={onOpen}
+      >
+        <Flex
+          opacity={0}
+          _hover={{ opacity: 1 }}
+          position={'absolute'}
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bg={'blackAlpha.700'}
+          transition={'all 0.3s ease'}
+          zIndex={1}
+          justifyContent={'center'}
+        ></Flex>
 
-      <Image
-        src={data?.imageURL}
-        alt='profile post'
-        w={'100%'}
-        h={'100%'}
-        objectFit={'cover'}
-      />
-    </GridItem>
+        <Image
+          src={data?.imageURL}
+          alt='profile post'
+          w={'100%'}
+          h={'100%'}
+          objectFit={'cover'}
+        />
+      </GridItem>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered={true}
+        size={{ base: '3xl', md: '5xl' }}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody bg={'black'} pb={5}>
+            <Flex
+              gap='4'
+              w={{ base: '90%', sm: '70%', md: 'full' }}
+              mx={'auto'}
+              maxH={'90vh'}
+              minH={'50vh'}
+            >
+              <Flex
+                borderRadius={4}
+                overflow={'hidden'}
+                border={'1px solid'}
+                borderColor={'whiteAlpha.300'}
+                flex={1.5}
+                justifyContent={'center'}
+                alignItems={'center'}
+              >
+                {<Image src={data?.imageURL} alt='profile post' />}
+              </Flex>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
