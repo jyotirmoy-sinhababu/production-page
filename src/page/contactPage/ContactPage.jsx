eba21b4f3dd94530a408c1aa125fee36baea5575
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-03-03 19:28:16 +0530

    contact page updated

--- src/page/contactPage/ContactPage.jsx
+++ src/page/contactPage/ContactPage.jsx
@@ -1,19 +1,42 @@
 import { Button, Flex, Heading, Link } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 
-import { FaFacebookSquare } from 'react-icons/fa';
+import { FaFacebookSquare, FaMobileAlt } from 'react-icons/fa';
 import { SiInstagram } from 'react-icons/si';
 import { MdMail } from 'react-icons/md';
 
 const ContactPage = () => {
+  const copyFunction = async (data) => {
+    try {
+      await navigator.clipboard.writeText(data);
+      alert('copied');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <Flex flexDir={'column'} alignItems={'center'} pt={'7%'} gap={'8px'}>
-        <Heading fontSize={'7xl'}>Let's work together.</Heading>
-        <Heading fontSize={'6xl'} opacity={'0.8'}>
+      <Flex
+        flexDir={'column'}
+        alignItems={'center'}
+        justifyContent={{ base: 'space-between', md: 'center' }}
+        mt={{ base: '12%', md: '7%' }}
+        gap={{ base: '27px', md: '8px' }}
+      >
+        <Heading fontSize={{ base: '4xl', md: '7xl' }}>
+          Let's work together.
+        </Heading>
+        <Heading fontSize={{ base: '4xl', md: '7xl' }} opacity={'0.6'}>
           Get in touch.
         </Heading>
-        <Flex>
+        <Flex
+          gap={'35px'}
+          mt={'13px'}
+          alignItems={'center'}
+          justifyContent={'space-around'}
+          w={'380px'}
+        >
           <Link
             fontSize={'42px'}
             background={'transparent'}
@@ -34,8 +57,25 @@ const ContactPage = () => {
           >
             <SiInstagram />
           </Link>
-          <Button onClick={{}}>
+          <Button
+            onClick={() => {
+              copyFunction('rickdas2982000@gmail.com');
+            }}
+            background={'transparent'}
+            fontSize={'56px'}
+            _hover={{ color: 'gray.500' }}
+          >
             <MdMail />
+          </Button>
+          <Button
+            onClick={() => {
+              copyFunction(7063282894);
+            }}
+            background={'transparent'}
+            fontSize={'44px'}
+            _hover={{ color: 'gray.500' }}
+          >
+            <FaMobileAlt />
           </Button>
         </Flex>
       </Flex>
